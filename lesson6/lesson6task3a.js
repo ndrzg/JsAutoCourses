250f916aa9a9971e16e1139aeec763347e665b6a
AndreyZ <dushesio@gmail.com>
2023-06-13 11:11:46 +0300

    added 3b task

--- lesson6/lesson6task3a.js
+++ lesson6/lesson6task3a.js
@@ -2,15 +2,15 @@ let number = 13
 let parts = 6
 
 let partsArray = [];
-let remaining = number;
+let remaining = number
   
     for (let i = 0; i < parts - 1; i++) {
-      let maxNumber = remaining;
-      let randNumber = Math.floor(Math.random() * (maxNumber + 1));
-      remaining -= randNumber;
-      partsArray.push(randNumber);
+      let maxNumber = remaining
+      let randNumber = Math.floor(Math.random() * (maxNumber + 1))
+      remaining -= randNumber
+      partsArray.push(randNumber)
     }
   
-    partsArray.push(remaining);
+    partsArray.push(remaining)
   
     console.log(partsArray)
